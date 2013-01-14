dd0f634af0f35715b74084f915b52833b2a15a1b
Dan <paulsson.dan@gmail.com>
2013-01-14 21:34:02 +0100

    saved a firststart of a thx page

---
+++ payment/tack.php
@@ -0,0 +1,47 @@
+<html>
+	<head>
+		<title>Produkter - Varukorg</title>
+		<link href="../html-mallar/style.css" type="text/css" rel="stylesheet" />
+		<meta charset="UTF-8" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<div class="logo">
+					<img src="../images/logo.jpg" />
+				</div>
+				<ul class="menu">
+					<li><a href="index.html">Hem</a></li>
+					<li><a href="about.html">Om oss</a></li>
+					<li><a href="products.html">Produkter</a></li>
+					<li><a href="contact.html">Kontakt</a></li>
+					<li><a href="profiles.html">Profilsida</a></li>
+				</ul>
+				<p class="login">
+					You are logged in as, <strong>Darth Vader</strong>! <button>Logout</button>
+				</p>
+			</div>
+
+			<div id="content">
+				<h1>Thanks for ordering</h1>
+
+				<div id="buyLeft">
+					We are pleased to say that your order has been sent. At the right you can see a shorty of your order.
+				</div>
+
+				<div id="buyRight">
+					<div class="buyBox">
+						
+						<p>Orderdetails:</p>
+						<p>Darth Vader, 1 st<br />
+							Totalprice: 1235 kr</p>
+
+					</div>	
+				</div>
+			</div>
+		</div>
+		<div id="footer">
+			<p>&copy Duderinos</p>
+		</div>
+	</body>
+</html>
