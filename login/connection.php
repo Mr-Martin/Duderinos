897fcba280c7603fc1bf9207e091e52afd74ca48
Dan Paulsson <paulsson.dan@gmail.com>
2013-01-15 09:49:34 +0100

    error report on connection

--- login/connection.php
+++ login/connection.php
@@ -2,7 +2,14 @@
 	// database connection
 	class Connection {
 		public function dbConnect() {
-			return new PDO("mysql:host=localhost; dbname=starwars_db", "root", "");
+			try {
+				$string = new PDO("mysql:host=localhost; dbname=starwars_db", "root", "");
+				$string->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+			catch(PDOException $e) {
+			    echo 'ERROR: ' . $e->getMessage();
+			}
+		return $string;
 		}
 	}
 ?>
