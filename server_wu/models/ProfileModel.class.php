6b5bdccca5334a4ca328848f65aa9d4eb46d0843
Dan Paulsson <paulsson.dan@gmail.com>
2013-01-22 09:18:39 +0100

    test with database connection to profile

--- server_wu/models/ProfileModel.class.php
+++ server_wu/models/ProfileModel.class.php
@@ -2,6 +2,13 @@
 
 class ProfileModel extends BaseModel{
 
+  // test with connection to database
+  private $session = $_SESSION['sess_id'];
+
+  private $listData = $this->db->prepare("SELECT * FROM users WHERE username=?");
+          $listData->bindParam(1, $session);
+          $listData->execute();
+/*
   private $listData = array(
         'nickName' => "Darth Vader",
         'profileName' => "Simon Quick",
@@ -11,7 +18,7 @@ class ProfileModel extends BaseModel{
         'nickGender' => "Male",
         'nickNat' => "Swedish",
         'nickAge' => "95"
-  );
+  ); */
 
   public function getProfileList(){
     return $this->listData;
