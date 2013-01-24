8dec6ce5416e4da677c44dc718e266bbe21889dc
Dan Paulsson <paulsson.dan@gmail.com>
2013-01-24 14:11:19 +0100

    profile update again after GIT fuckup!

--- server_wu/templates/profile.list.tpl.php
+++ server_wu/templates/profile.list.tpl.php
@@ -1,8 +1,8 @@
-<h3>Profile:</h3><h1 class="profileName"><?php echo $profileName; ?></h1>	
+<h3>Profile:</h3><h1 class="profileName"><?php echo $fullname; ?></h1>	
 
 				<div id="profile">
 					<div class="profileBox">
-						<img src="../images/lordsimon.jpg" width="300px;" height="300x;"/>
+						<img src="../images/<?php echo $userimage; ?>" width="300px;" height="300x;"/>
 
 						<ul class="profileBoxLabels">
 							<li><strong>Nickname</strong></li>
@@ -14,16 +14,16 @@
 						</ul>
 
 						<ul class="profileBoxInfo">
-							<li><strong><?php echo $nickName; ?></strong></li>
-							<li><a href="#"><?php echo $nickStatus; ?></a></li>
-							<li><?php echo $nickGender; ?></li>
-							<li><?php echo $nickAge; ?></li>
-							<li><?php echo $nickNat; ?></li>
+							<li><strong><?php echo $username; ?></strong></li>
+							<li><a href="#"><?php echo $status; ?></a></li>
+							<li><?php echo $gender; ?></li>
+							<li><?php echo $age; ?></li>
+							<li><?php echo $nationality; ?></li>
 							
 						</ul>
 
 						<h3>About Me:</h3>
-						<p class="profileDesc"><?php echo $profileDesc; ?></p>
+						<p class="profileDesc"><?php echo $about; ?></p>
 
 						<br>
 						<h3>My inventory:</h3>
