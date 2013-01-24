cae706b3acee5ec307bfba8e4a907d236a1311a6
Martin Nilsson <mune2000@msn.com>
2013-01-24 14:51:07 +0100

    NOW ITS WORKING AGAIN!!!...?

--- server_wu/models/ProfileModel.class.php
+++ server_wu/models/ProfileModel.class.php
@@ -6,22 +6,18 @@ class ProfileModel extends BaseModel{
 
   // get profile from database with right username
   protected function getProfileListData($user){
-    $rows = $this->db->get("SELECT userName, userimage, fullName, status, age, nationality, about FROM user WHERE userName = 'Duderino'");
+    $rows = $this->db->get("SELECT userName, userimage, fullName, status, age, nationality, about FROM user WHERE userName = '$username'");
     if(count($rows)>0){
       return $rows[0];
     }
-  
     return array();
+  }
 
-    public function getProfileList($user){     
+  public function getProfileList($user){     
       if(!$this->listData){
         $this->listData = $this->getProfileListData($user);
       }
-    return $this->listData;
-    }
-
-    public function getProfileList(){
       return $this->listData;
     }
-  }
+
 }
--- server_wu/templates/profile.list.tpl.php
+++ server_wu/templates/profile.list.tpl.php
@@ -1,34 +1,35 @@
-<h3>Profile:</h3><h1 class="profileName"><?php echo $fullname; ?></h1>	
+<h3>Profile:</h3><h1 class="profileName"><?php echo $fullName; ?></h1>	
 
 				<div id="profile">
 					<div class="profileBox">
-						<img src="../images/<?php echo $userimage; ?>" width="300px;" height="300x;"/>
+						<img src="images/<?php echo $userimage; ?>" width="300px;" height="300x;"/>
 
-						<ul class="profileBoxLabels">
-							<li><strong>Nickname</strong></li>
-							<li><strong>Status</strong></li>
-							<li><strong>Gender</strong></li>
-							<li><strong>Age</strong></li>
-							<li><strong>Nationality</strong></li>
+						<!-- <ul class="profileBoxLabels"> -->
+						<ul>
+							<li><strong>Nickname</strong>: <?php echo $userName; ?></li>
+							<li><strong>Status</strong>: <?php echo $status; ?></li>
+							<li><strong>Gender</strong>: <?php echo $gender; ?></li>
+							<li><strong>Age</strong>: <?php echo $age; ?></li>
+							<li><strong>Nationality</strong>: <?php echo $nationality; ?></li>
 							
 						</ul>
 
-						<ul class="profileBoxInfo">
-							<li><strong><?php echo $username; ?></strong></li>
-							<li><a href="#"><?php echo $status; ?></a></li>
+						<!-- <ul class="profileBoxInfo">
+							<li><strong><?php echo $userName; ?></strong></li>
+							<li><?php echo $status; ?></li>
 							<li><?php echo $gender; ?></li>
 							<li><?php echo $age; ?></li>
 							<li><?php echo $nationality; ?></li>
 							
-						</ul>
-
+						</ul> -->
+						<br />
 						<h3>About Me:</h3>
 						<p class="profileDesc"><?php echo $about; ?></p>
 
 						<br>
 						<h3>My inventory:</h3>
 						<div class="inventory">
-							<img src="../images/price_chart.jpg" width="450px;" height="155x;"/>
+							<img src="images/price_chart.jpg" width="450px;" height="155x;"/>
 						</div>
 					
 					</div>
