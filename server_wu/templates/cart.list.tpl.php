472ca4dd18b5ebe388e1232a7e8b07d197696d74
Martin Nilsson <mune2000@msn.com>
2013-01-28 15:44:02 +0100

    Update

--- server_wu/templates/cart.list.tpl.php
+++ server_wu/templates/cart.list.tpl.php
@@ -1,13 +1,4 @@
-<?php
-echo "Hello";
-/*
-	if($item['item_id'] && $item['item_price'] && $item['item_desc']){
-		echo '<li>';
-		echo $item['productName'] . '<br />';
-		echo $item['productPrice'] . '<br />';
-		echo '<hr><br />';
-		echo '</li>';
-	} else {
-		echo 'Cart empty';
-	}
-*/
+<p>Cart</p>
+	<ul>
+		<
+	</ul>
--- server_wu/templates/chrome.initial.tpl.php
+++ server_wu/templates/chrome.initial.tpl.php
@@ -8,10 +8,6 @@
 
         </ul>
 
-        <ul id="cartbasket">
-    
-        </ul>
-        
         <form action="#" method="post" class="login" id="userlogin">
           <p>User: </p><input type="text" name="username" />
           <p>Pass: </p><input type="text" name="password" />
