7a81d8a4228b0361c410882e519bc91b246910b9
Simon Quick <quick.simon@gmail.com>
2013-02-13 12:22:17 +0100

    Added feedback

--- server_wu/templates/chrome.initial.tpl.php
+++ server_wu/templates/chrome.initial.tpl.php
@@ -1,4 +1,37 @@
     <div id="wrapper">
+      <script>
+  $(document).ready(function(){
+    $('#feedback-trigger').click(function(event){
+      event.preventDefualt
+    $(this).next('#feedback-content').slideToggle();
+    $(this).toggleClass('active');          
+    
+    if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
+      else $(this).find('span').html('&#x25BC;')
+    })
+    });
+      </script>
+      <nav>
+        <ul>
+        <li id="feedback">
+          <a id="feedback-trigger" href="#">
+            Send us feedback! <span>▼</span>
+          </a>
+        <div id="feedback-content">
+        <form>
+          <fieldset id="inputs">
+            <input id="username" type="email" name="Email" placeholder="Your email adress!" required>   
+            <textarea id="textarea" type="text" name="Textfield" placeholder="Your feedback here!" required></textarea>
+          </fieldset>
+          <fieldset id="actions">
+            <input type="submit" id="submit" value="Send">
+            <label><input type="checkbox" checked="checked"> Send me a reply!</label>
+          </fieldset>
+        </form>
+      </div>                     
+      </li>
+      </ul>
+      </nav>
       
       <div id="header">
         <div id="userinfo"></div>
